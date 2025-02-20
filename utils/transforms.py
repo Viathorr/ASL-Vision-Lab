925462190fc22cf9d6ac45f7ac90b19a511c66e7
Viathorr <142295754+Viathorr@users.noreply.github.com>
2025-02-20 16:33:55 +0200

    Add resizing transformation to ASL MNIST training and test datasets

--- utils/transforms.py
+++ utils/transforms.py
@@ -2,6 +2,7 @@ from torchvision import transforms
 
 
 asl_mnist_train_transforms = transforms.Compose([
+  transforms.Resize((28, 28)),
   transforms.RandomHorizontalFlip(),
   transforms.RandomRotation(10, interpolation=transforms.InterpolationMode.BILINEAR, fill=128),
   transforms.ToTensor(), 
@@ -9,6 +10,7 @@ asl_mnist_train_transforms = transforms.Compose([
 ])
 
 asl_mnist_test_transforms = transforms.Compose([
+  transforms.Resize((28, 28)),
   transforms.ToTensor(), 
   transforms.Normalize(mean=(0.1307,), std=(0.3081,))
 ])
